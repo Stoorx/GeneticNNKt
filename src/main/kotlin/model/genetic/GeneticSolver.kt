bfbc3b5054b0a29aa08ece85959a0baf4ae76d1b
Stoorx <antiproton2013@yandex.ru>
2019-05-17 22:46:00 +0300

    Add evolution step

--- src/main/kotlin/model/genetic/GeneticSolver.kt
+++ src/main/kotlin/model/genetic/GeneticSolver.kt
@@ -1,7 +1,24 @@
 package model.genetic
 
+import util.SingleRandom
+
 class GeneticSolver {
     companion object {
         var mutationRate: Double = 0.5
+    }
+
+    val population: ArrayList<Model> = ArrayList()
+
+    fun evolutionStep() {
+        val newPopulation: ArrayList<Model> = ArrayList()
+        newPopulation.addAll(population)
+
+        population.forEach {
+            newPopulation.add(
+                it.crossover(population[SingleRandom.nextInt(population.size)])
+            )
+        }
+
+        // TODO: sort and select
     }
 }
