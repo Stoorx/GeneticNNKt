50a220a0a92840aa0122b4f656145a58f81aea14
Stoorx <antiproton2013@yandex.ru>
2019-05-17 22:39:55 +0300

    Add methods to Model

--- src/main/kotlin/model/genetic/Model.kt
+++ src/main/kotlin/model/genetic/Model.kt
@@ -1,9 +1,28 @@
 package model.genetic
 
+import model.neural.InputNeuron
 import model.neural.NeuronLayer
 
 
 class Model {
     val layers: ArrayList<NeuronLayer> = ArrayList()
+    fun setInput(values: DoubleArray) {
+        layers[0].neurons.forEachIndexed { index, neuron ->
+            if (neuron is InputNeuron)
+                neuron.value = values[index]
+        }
+    }
+
+    fun calculate(values: DoubleArray): DoubleArray {
+        setInput(values)
+        val lastLayer: NeuronLayer = layers.last()
+        val outputs = ArrayList<Double>()
+
+        lastLayer.neurons.forEach {
+            outputs.add(it.calculate())
+        }
+
+        return outputs.toDoubleArray()
+    }
 }
 
